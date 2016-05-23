b78f4e6a93d26ef9f490d870d7e89e482fc209a2
Thomas Morris <thomas.morris@3pillarglobal.com>
2016-05-23 12:12:56 +0300

    add routes for add/removing from favorites

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,10 @@ Rails.application.routes.draw do
     mount_devise_token_auth_for 'User', at: 'auth'
 
     namespace :v1 do
-      resources :recipes
+      resources :recipes do 
+        post "favorites" => "favorite_recipes#create"
+        delete "favorites" => "favorite_recipes#destroy"
+      end
     end
   end
 end
